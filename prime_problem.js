57c8f2d4ea78f2b3b8727d7236396cae6a928a0a
unknown <shreyanksupare140499@gmail.com>
2022-12-03 13:28:11 +0530

    primeno.

---
+++ prime_problem.js
@@ -0,0 +1,16 @@
+    let num =43;
+    let count=0;
+    for(let a=0;a<=num;a++){
+        if(num%a===0)
+            count++
+        }
+        if(count==1)
+        {
+        console.log("Yes")
+        }
+        else
+        {
+        console.log("No")
+        }
+    
+
