d4617b2ea198b22b6ff236d6485e10feb9126a74
unknown <shreyanksupare140499@gmail.com>
2022-12-04 18:15:21 +0530

    palindrome

---
+++ palindrome.js
@@ -0,0 +1,17 @@
+
+let num = 1221
+let bag = "";{
+    for(i=num[0];i>num.length;i--){
+        bag += num[i] 
+    }
+    
+        if(num[i]==bag)
+        {
+          console.log("Yes")
+        }
+         else 
+        {
+        console.log("No")
+        }
+
+}
